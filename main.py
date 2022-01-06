6c5482ad2dceafab1349670b95523aed47bc0870
Benjamin.Chinwe <chisoftltd@gmail.com>
2022-01-06 23:24:43 +0000

    Initial commit - Sunset Times API

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 6.458366
+MY_LONG = -7.546388
+MY_FORMT = 0
+
+parameters = {
+    "lat": MY_LAT,
+    "long": MY_LONG,
+    "formatted": MY_FORMT,
+}
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+print(sunrise.split("T"))
+print(sunset.split("T"))
+
+sunrise_list = sunrise.split("T")
+
+current_date = datetime.now()
+print(current_date)
